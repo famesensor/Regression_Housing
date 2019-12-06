c65f88534943fb8086dd0c528a945606bfafccf2
famesensor <famfew25@gmail.com>
2019-12-06 21:15:23 +0700

    Update server

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, send_from_directory, send_file
 from predict_api import predict_price
+from sklearn import linear_model
 import pickle
 
 app = Flask(__name__)
@@ -11,9 +12,15 @@ def index() :
 
 @app.route("/predict", methods=["POST"])
 def predict() :
-    data = 0 
-    predict_price(data, load_model)
-    return render_template("predict.html")
+    data = []
+    data.append(request.form['room'])
+    data.append(request.form['bedroom'])
+    data.append(request.form['carspots'])
+    data.append(request.form['type'])
+    data.append(request.form['region'])
+    data.append(request.form['year'])
+    # predict_p = predict_price(data, load_model)
+    return render_template("predict.html", prediction = data)
 
 if __name__ == "__main__" :
     app.run(port="3300",debug=True)
