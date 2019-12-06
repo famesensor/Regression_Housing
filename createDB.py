22712d896293ef64f677a75f3854a4012d5ac638
famesensor <famfew25@gmail.com>
2019-12-07 00:34:29 +0700

    Add Database

---
+++ createDB.py
@@ -0,0 +1,6 @@
+from server import db
+from static.models.DB import Data
+
+db.create_all()
+
+print("DB created.")
--- server.py
+++ server.py
@@ -1,9 +1,13 @@
-from flask import Flask, render_template, request, send_from_directory, send_file
+from flask import Flask, render_template, request, send_from_directory
 from predict_api import predict_price
 from sklearn import linear_model
+from flask_sqlalchemy import SQLAlchemy
+from server import db
+from static.models.DB import Data
 import pickle
 
 app = Flask(__name__)
+app.config.from_object('config')
 load_model = pickle.load(open("static/models/model.pkl", "rb"))
 
 @app.route("/", methods=["GET"])
@@ -19,6 +23,14 @@ def predict() :
     data.append(request.form['type'])
     data.append(request.form['region'])
     data.append(request.form['year'])
+    data_enter = Data(notes = data)
+    if request.method == 'POST' :
+        try:     
+            db.session.add()
+            db.session.commit()        
+            db.session.close()
+        except:
+            db.session.rollback()
     # predict_p = predict_price(data, load_model)
     return render_template("predict.html", prediction = data)
 
