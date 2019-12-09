e1a62527566d117ace3666551453625a92568a29
famesensor <famfew25@gmail.com>
2019-12-09 15:51:33 +0700

    Update index and view

--- app/view.py
+++ app/view.py
@@ -14,19 +14,20 @@ def index() :
 @app.route("/predict", methods=["POST"])
 def predict() :
     if request.method == "POST" :
-        df_user = request.form.to_dict()
-        df_user = list(df_user.values())
-        # df_user = request.form['room']
-        # df_user = request.form['date']
-        # df_user = request.form['distance']
-        # df_user = request.form['bedroom']
-        # df_user = request.form['bathroom']
-        # df_user = request.form['parkinglots']
-        # df_user = request.form['buildingarea']
-        # df_user = request.form['councilarea']
-        # df_user = request.form['age']
-        # df_user = request.form['type']
-        # df_user = request.form['region']
+        df_user = []
+        # df_user = request.form.to_dict()
+        # df_user = list(df_user.values())
+        df_user.append(request.form['room'])
+        df_user.append(request.form['date'])
+        df_user.append(request.form['distance'])
+        df_user.append(request.form['bedroom'])
+        df_user.append(request.form['bathroom'])
+        df_user.append(request.form['parkinglots'])
+        df_user.append(request.form['buildingarea'])
+        df_user.append(request.form['councilarea'])
+        df_user.append(request.form['age'])
+        df_user.append(request.form['type'])
+        df_user.append(request.form['region'])
         predict_p = predict_price(df_user, model)
         # predict_p = 10000
         df_user.append(predict_p)
@@ -39,7 +40,22 @@ def predict() :
             print("Success")
         except:
             db.session.rollback()
+
+    # return jsonify({'price' : predict_p})
     return render_template("predict.html", predict = df_user)
+
+# @app.route('/process', methods=['POST'])
+# def process():
+
+# 	email = request.form['room']
+# 	name = request.form['date']
+
+# 	if name and email:
+# 		newName = name[::-1]
+
+# 		return jsonify({'name' : newName})
+
+# 	return jsonify({'error' : 'Missing data!'})
 
 if __name__ == "__main__" :
     app.run(port="3300",debug=True)
