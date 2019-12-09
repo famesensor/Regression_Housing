2f2f4136c2c82a94cefe008498a3cd6ebb375eb2
famesensor <famfew25@gmail.com>
2019-12-09 22:23:46 +0700

    Update predict_api view and DB

--- app/predict_api.py
+++ app/predict_api.py
@@ -22,6 +22,7 @@ def preparation(data) :
     df = df.merge(df_type)
     df = df.merge(df_region)
     df = df.drop(['Type','Region'], axis=1)
+    # print(df)
     return df
 
 def predict_price(data ,model) :
@@ -29,3 +30,50 @@ def predict_price(data ,model) :
     df_user = preparation(data)
     predict_p = model.predict(poly_reg.fit_transform(df_user))
     return predict_p[0]
+
+def changedata(data) :
+    result = data
+    df_type = {'h':'House', 't':'Townhouse', 'u':'Unit'}
+    df_con = {1:'Banyule City Council',
+        2:'Bayside City Council',
+        3:'Boroondara City Counci',
+        4:'Brimbank City Council',
+        5:'Cardinia Shire Council',
+        6:'Casey City Council',
+        7:'Darebin City Council',
+        8:'Frankston City Council',
+        9:'Glen Eira City Council',
+        10:'Greater Dandenong City Council',
+        11:'Hobsons Bay City Council',
+        12:'Hume City Council',
+        13:'Kingston City Council',
+        14:'Knox City Council',
+        15:'Macedon Ranges Shire Council',
+        16:'Manningham City Council',
+        17:'Maribyrnong City Council',
+        18:'Maroondah City Council',
+        19:'Melbourne City Council',
+        20:'Melton City Council',
+        21:'Mitchell Shire Council',
+        22:'Monash City Council',
+        23:'Moonee Valley City Council',
+        24:'Moorabool Shire Council',
+        25:'Moreland City Council',
+        26:'Nillumbik Shire Council',
+        27:'Port Phillip City Council',
+        28:'Stonnington City Council',
+        29:'Whitehorse City Council',
+        30:'Whittlesea City Council',
+        31:'Wyndham City Council',
+        32:'Yarra City Council',
+        33:'Yarra Ranges Shire Council'
+    }
+    for k,v in df_type.items() :
+        if result[9] == k :
+            result[9] = v
+            break
+    for k,v in df_con.items() :
+        if int(result[7]) == k :
+            result[7] = v
+            break
+    return result
--- app/view.py
+++ app/view.py
@@ -1,6 +1,6 @@
 from app import app, db
 from flask import Flask, render_template, request, redirect, url_for, jsonify, get_template_attribute
-from app.predict_api import predict_price
+from app.predict_api import predict_price, changedata
 from sklearn import linear_model
 from app.Db import Data
 import pickle
@@ -31,15 +31,16 @@ def predict() :
         predict_p = predict_price(df_user, model)
         # predict_p = 10000
         df_user.append(predict_p)
-        # print(df_user)
+        df_user = changedata(df_user)
+        print(df_user)
         data_enter = Data(detail = df_user)
-        try : 
-            db.session.add(data_enter)
-            db.session.commit()        
-            db.session.close()
-            print("Success")
-        except:
-            db.session.rollback()
+        # try : 
+        db.session.add(data_enter)
+        db.session.commit()        
+        db.session.close()
+        print("Success")
+        # except:
+        #     db.session.rollback()
 
     # return jsonify({'price' : predict_p})
     return render_template("predict.html", predict = df_user)
